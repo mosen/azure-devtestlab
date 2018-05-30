729eaea16f14a6c102ff9e1148a005c18b49d3a9
Mosen <mosen@users.noreply.github.com>
2018-05-30 21:52:54 +1000

    tomcat has no systemd unit so added one

--- Artifacts/linux-tomcat/linux_install_tomcat.sh
+++ Artifacts/linux-tomcat/linux_install_tomcat.sh
@@ -18,6 +18,7 @@ wget http://apache.mirror.serversaustralia.com.au/tomcat/tomcat-8/v8.0.52/bin/ap
 mkdir /opt/tomcat
 tar xvf apache-tomcat-8.0.52.tar.gz -C /opt/tomcat --strip-components=1
 
+echo "Adjusting permissions for the tomcat user and group"
 cd /opt/tomcat
 chgrp -R "${2}" /opt/tomcat
 chmod -R g+r conf
@@ -25,6 +26,39 @@ chmod g+x conf
 
 chown -R "${1}" webapps/ work/ temp/ logs/
 
+echo "Creating the systemd unit at /etc/systemd/system/tomcat.service"
+cat > /etc/systemd/system/tomcat.service <<-EOF
+[Unit]
+Description=Apache Tomcat 8.5.x Web Application Container
+Wants=network.target
+After=syslog.target network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=/usr/java/default
+Environment=JRE_HOME=$JAVA_HOME/jre
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment=CATALINA_BASE=/opt/tomcat
+Environment='CATALINA_OPTS=-server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Xms256M -Xmx512M'
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+SuccessExitStatus=143
+
+User=${1}
+Group=${2}
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Reloading systemd"
 systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
